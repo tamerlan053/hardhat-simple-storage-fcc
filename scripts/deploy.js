f746ad8c605bf2fa61f39d495c3b291e08f7f1eb
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-06-12 23:56:42 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,7 @@ const { ethers, run, network } = require("hardhat");
 async function main() {
   const deployedContract = await ethers.deployContract("SimpleStorage");
   console.log("Deploying contract...");
-  await deployedContract.waitForDeployment();
+  await deployedContract.deployed();
   console.log("SimpleStorage Contract Address:", await deployedContract.getAddress());
   if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
     await deployedContract.deployTransaction.wait(6)
