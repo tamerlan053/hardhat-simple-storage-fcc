9fb65982c73653f3ba66cb22b4595354ec4cff1a
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-06-14 12:23:49 +0200

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,13 +6,13 @@ async function main() {
   await deployedContract.waitForDeployment();
   console.log("SimpleStorage Contract Address:", await deployedContract.getAddress());
   if (network.config.chainId == 11155111 && process.env.ETHERSCAN_API_KEY) {
-    await deployedContract.deployTransaction.wait(5)
-    await verify(deployedContract.address, []);
+  await deployedContract.deployTransaction.wait(5)
+   await verify(deployedContract.address, []);
   }
-    const currentValue = await deployedContract.retrieve()
+  const currentValue = await deployedContract.retrieve()
   console.log(`Current value is: ${currentValue}`)
   
-    const transactionResponse = await deployedContract.store(7)
+  const transactionResponse = await deployedContract.store(7)
   await transactionResponse.wait(1)
   const updatedValue = await deployedContract.retrieve()
   console.log(`Updated value is: ${updatedValue}`)
