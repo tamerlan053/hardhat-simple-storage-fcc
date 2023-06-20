0eaf79a0ab6d05b350331e5ac02cbe489e1cbf8b
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2023-06-20 23:52:00 +0200

    Update block-number.js

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -2,6 +2,7 @@ const { task } = require("hardhat/config")
 
 task("block-number", "Prints the current block number").setAction(
     async (taskArhs, hre) => {
-        const blockNumber = hre.ethers.provider.getBlockNumber()
+        const blockNumber = await hre.ethers.provider.getBlockNumber()
+        console.log(blockNumber)
     }
 )
